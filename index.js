47db54c6d0e43dce93ce21c7a58c2cd23356951b
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-10-18 17:21:15 +0530

    folder structure improve

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const connectToMongo = require('./db');
+const connectToMongo = require('./src/config/db');
 const express = require('express')
 var cors = require('cors')
 const app = express()
@@ -10,11 +10,11 @@ app.use(cors())
 
 app.use(express.json())  //middle ware to use req.body
 
-app.use('/api/auth', require('./Routes/auth'))
-app.use('/api/projects', require('./Routes/project'))
-app.use('/api/certificates', require('./Routes/certificates'))
-app.use('/api/quotes', require('./Routes/quote'))
-app.use('/api/resume', require('./Routes/resume'))
+app.use('/api/auth', require('./src/Routes/auth'))
+app.use('/api/projects', require('./src/Routes/project'))
+app.use('/api/certificates', require('./src/Routes/certificates'))
+app.use('/api/quotes', require('./src/Routes/quote'))
+app.use('/api/resume', require('./src/Routes/resume'))
 
 app.listen(port, () => {
     console.log(`portfolio server is running on port ${port}`)
