7fcfa51e34645f7f84f528d9f88f5bc672fca4d8
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-11-13 20:38:27 +0530

    package updated

--- src/ThirdParty_API/SpotifyLib.js
+++ src/ThirdParty_API/SpotifyLib.js
@@ -1,3 +1,4 @@
+const fetch = require('isomorphic-unfetch')
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
