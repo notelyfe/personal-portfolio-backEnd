5f6c53c7e313a67fb770d4a17e3ff3e5721c3217
Ankesh Kumar <notelyfe@gmail.com>
2023-04-22 11:30:56 +0530

    spotify bug fix

--- src/Routes/Spotify.js
+++ src/Routes/Spotify.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const getNowPlaying = require('../ThirdParty_API/SpotifyLib')
 
-router.post('/getNowPlaying', async (req, res) =>{
-    const nowPlaying = await getNowPlaying()
+router.post('/getNowPlaying', async (req, res) => {
 
-    res.json(nowPlaying)
+    try {
+        const nowPlaying = await getNowPlaying()
+
+        res.json(nowPlaying)
+    } catch (error) {
+        res.status(500).json({ message: "Spotify Server Error" })
+    }
 })
 
 module.exports = router;
