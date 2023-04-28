667f1b617a61c847b941be3427e1930c8759b4c4
Ankesh Kumar <notelyfe@gmail.com>
2023-04-28 19:47:48 +0530

    cors bug fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8000;
 
-const whitelist = ['https://notelyfe.select']
+const whitelist = ['https://notelyfe.select', 'https://admin.notelyfe.select']
 
 const corsOptions = {
     origin: (origin, callback) => {
